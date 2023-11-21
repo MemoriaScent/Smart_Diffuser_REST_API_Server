d992eeca07556b30cf0085ef823fb3d9507bd219
mQueue <tomorrow9913@gmail.com>
2023-11-21 16:38:55 +0900

    Feat: 서버 생존 확인 용 echo api
    -

--- main.py
+++ main.py
@@ -23,6 +23,15 @@ app.include_router(admin.router)
 # routers
 app.include_router(user.router)
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/{echo}")
+async def echo(echo: str):
+    return {"echo": echo}
+
 
 if __name__ == "__main__":
     uvicorn.run(app='main:app', host="0.0.0.0", port=80, workers=4)
